9318b47ab49e4c0cb40993639491016a1e42eac5
Ryan Marks <Ryan@Sigmabot.com>
2015-11-22 16:21:32 -0500

    Added all functionalities to Page 8

--- src/ItemService.java
+++ src/ItemService.java
@@ -3,10 +3,46 @@ import java.util.Hashtable;
 
 public class ItemService {
 
+
+    public boolean isReadable(int serial){
+        return true;
+    }
+
+    public boolean available(int serial, int amount){
+        return amount > 0;
+    }
+
+    public String getName(int serial){
+        return "Item name";
+    }
+
+    public String getType(int serial){
+        return "Type";
+    }
+
+    public String addToCart(int serial, int amount){
+        StringBuilder sb = new StringBuilder();
+        sb.append(amount);
+        sb.append(" ");
+        sb.append(getName(serial));
+        sb.append(" ");
+        sb.append(getType(serial));
+        if (amount != 1)
+            sb.append("s");
+        sb.append(" ");
+        if (available(serial,amount)){
+            sb.append("successfully added to your cart");
+        }else{
+            sb.append("could not be added to your cart");
+        }
+        return sb.toString();
+    }
+
     ArrayList<Readable> getReadables(){
         ArrayList<Readable> books = new ArrayList<>();
         Readable book = new eBook(1,"This is a book","Book Writer",100,10);
         books.add(book);
+
 
         return books;
 
--- src/Page8.java
+++ src/Page8.java
@@ -1,5 +1,7 @@
+import com.sun.deploy.util.SyncAccess;
+
 import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.Scanner;
 
 public class Page8 extends Page{
 
@@ -14,6 +16,9 @@ public class Page8 extends Page{
 
     @Override
     public int render() {
+
+        System.out.println("Readables");
+        System.out.println();
         //                  12345123451234512345123451234512345123451234512345123451234512345123451234512345
         System.out.println("S.No Name of the Book    Author              Price($)  Quantity in Store   Type");
         ArrayList<Readable> readables = is.getReadables();
@@ -21,6 +26,59 @@ public class Page8 extends Page{
             System.out.println(r.getInfo());
         }
 
-        return 0;
+
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        int serial = -1;
+        do {
+            System.out.println("Press -1 to return to the previous menu.");
+            System.out.print("Choose your option: ");
+            String input = sc.next();
+            if (input.equals("-1")){
+                return 6; // Goto page 6, the category page
+            }
+            try{
+                serial = Integer.parseInt(input);
+                if (is.isReadable(serial)){
+                    loop = false;
+                }else{
+                    System.out.println("Please enter the serial number of a readable");
+                }
+            }catch (NumberFormatException e){
+                System.out.println("Please enter an integer serial number");
+            }
+        } while (loop);
+
+        int quantity = -1;
+        loop = true;
+        do {
+            System.out.print("Enter quantity: ");
+            String input = sc.next();
+            if (input.equals("-1")){
+                return 6; // Goto page 6, the category page
+            }
+            try{
+                quantity = Integer.parseInt(input);
+                if (is.available(serial,quantity)){
+                    loop = false;
+                }else{
+                    System.out.println("Please enter an amount you can actually purchase");
+                }
+            }catch (NumberFormatException e){
+                System.out.println("Please enter an integer amount");
+            }
+        } while (loop);
+
+        String result = is.addToCart(serial,quantity);
+        System.out.println(result);
+        System.out.println();
+        do{
+            System.out.print("Press -2 to Continue Shopping or Press 0 to check out:");
+            String input = sc.next();
+            if (input.equals("0"))
+                return 10;
+            else if (input.equals("-2"))
+                return 6;
+        }while (true);
     }
 }
