c769ee55c609fbb37c9e2516fc666b7aab1e1033
Nishanth Balamohan <balamon@mcmaster.ca>
2015-11-24 04:14:52 -0500

    added comments to mp3

--- src/MP3.java
+++ src/MP3.java
@@ -3,12 +3,12 @@ public class MP3 extends Audio {
     public MP3(int serial, String itemName, String artist, int itemPrice, int itemQuantity) {
         super(serial, itemName, artist, itemPrice, itemQuantity);
     }
-
+    //Returns the type as "MP3"
     @Override
     public String getType() {
         return "MP3";
     }
-
+    //Notifies user if they are attempting to buy more of a product that what is available.
     @Override
     public Item buy(int amount) {
         if (amount > quantity)
@@ -16,7 +16,7 @@ public class MP3 extends Audio {
         quantity -= amount;
         return new MP3(sNo,getName(),artistName, getBasePrice(),amount);
     }
-
+    //Adds "MP3" to the end of the implementation of getInfo
     @Override
     public String getInfo() {
         return super.getInfo()+"MP3";
